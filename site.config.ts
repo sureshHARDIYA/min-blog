77f88629e06819c251d53d7742059c845846488a
Suresh KUMAR Mukhiya <itsmeskm99@gmail.com>
2023-08-26 16:45:19 +0200

    Add config

--- site.config.ts
+++ site.config.ts
@@ -22,7 +22,7 @@ export default siteConfig({
   linkedin: 'sureshhardiya',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'channel/UCJYkHhdWattnWsT4GfScztQ',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
