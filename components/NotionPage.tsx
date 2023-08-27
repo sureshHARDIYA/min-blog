eb6bdc9cd13ee6dd7388c226c9f9b174636e3aa4
Suresh KUMAR Mukhiya <itsmeskm99@gmail.com>
2023-08-27 13:36:58 +0200

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,31 +1,31 @@
-import * as React from 'react'
-import dynamic from 'next/dynamic'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-
 import cs from 'classnames'
+import Link from 'next/link'
+import * as React from 'react'
+import Image from 'next/image'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
 import { PageBlock } from 'notion-types'
-import { formatDate, getBlockTitle, getPageProperty } from 'notion-utils'
-import BodyClassName from 'react-body-classname'
-import { NotionRenderer } from 'react-notion-x'
-import TweetEmbed from 'react-tweet-embed'
 import { useSearchParam } from 'react-use'
+import TweetEmbed from 'react-tweet-embed'
+import { NotionRenderer } from 'react-notion-x'
+import BodyClassName from 'react-body-classname'
+import { formatDate, getBlockTitle, getPageProperty } from 'notion-utils'
 
-import * as config from '@/lib/config'
 import * as types from '@/lib/types'
+import * as config from '@/lib/config'
 import { mapImageUrl } from '@/lib/map-image-url'
-import { getCanonicalPageUrl, mapPageUrl } from '@/lib/map-page-url'
 import { searchNotion } from '@/lib/search-notion'
 import { useDarkMode } from '@/lib/use-dark-mode'
+import { getCanonicalPageUrl, mapPageUrl } from '@/lib/map-page-url'
 
 import { Footer } from './Footer'
-import { GitHubShareButton } from './GitHubShareButton'
 import { Loading } from './Loading'
-import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
-import { PageAside } from './PageAside'
 import { PageHead } from './PageHead'
+import { PageAside } from './PageAside'
+import { NotionPageHeader } from './NotionPageHeader'
+import { GitHubShareButton } from './GitHubShareButton'
+
 import styles from './styles.module.css'
 
 // -----------------------------------------------------------------------------
@@ -280,7 +280,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
         pageAside={pageAside}
         footer={footer}
       />
-
       <GitHubShareButton />
     </>
   )
